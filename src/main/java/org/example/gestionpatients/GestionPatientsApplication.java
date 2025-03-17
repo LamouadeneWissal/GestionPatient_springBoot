d6e9d37dbc3c4adbf2ee152207064b316addf8c0
LamouadeneWissal <wisslamouadene@gmail.com>
2025-03-17 10:54:25 +0000

    couche dao commit

--- src/main/java/org/example/gestionpatients/GestionPatientsApplication.java
+++ src/main/java/org/example/gestionpatients/GestionPatientsApplication.java
@@ -1,13 +1,33 @@
 package org.example.gestionpatients;
 
+import org.example.gestionpatients.entities.Patient;
+import org.example.gestionpatients.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GestionPatientsApplication {
+import java.util.Date;
 
+@SpringBootApplication
+public class GestionPatientsApplication implements CommandLineRunner {
+
+    @Autowired
+    private PatientRepository patientRepository;
     public static void main(String[] args) {
         SpringApplication.run(GestionPatientsApplication.class, args);
     }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null, "ahmed" , new Date() , false , 24));
+        patientRepository.save(new Patient(null, "Asmae" , new Date() , true , 16));
+        patientRepository.save(new Patient(null, "Wissal" , new Date() , false , 22));
+
+
+    }
+
 
 }
