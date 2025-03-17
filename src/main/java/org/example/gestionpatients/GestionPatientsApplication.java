e1eb0c2f5c1c1f8b3412b0a3b15eb502b54f730d
LamouadeneWissal <wisslamouadene@gmail.com>
2025-03-17 10:09:02 +0000

    Innit commit

---
+++ src/main/java/org/example/gestionpatients/GestionPatientsApplication.java
@@ -0,0 +1,13 @@
+package org.example.gestionpatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPatientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionPatientsApplication.class, args);
+    }
+
+}
