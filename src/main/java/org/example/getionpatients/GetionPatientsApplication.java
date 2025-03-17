36536b46549e205764c486685cf4aa7c2ca9a38a
LamouadeneWissal <wisslamouadene@gmail.com>
2025-03-17 09:32:29 +0000

    creation du projet

---
+++ src/main/java/org/example/getionpatients/GetionPatientsApplication.java
@@ -0,0 +1,13 @@
+package org.example.getionpatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetionPatientsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetionPatientsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/getionpatients/GetionPatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.getionpatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetionPatientsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
