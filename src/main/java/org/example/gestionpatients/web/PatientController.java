433de94375b286784d0a81e58cb5334d618d4833
LamouadeneWissal <wisslamouadene@gmail.com>
2025-03-26 21:53:16 +0000

    Second Interface commit

--- src/main/java/org/example/gestionpatients/web/PatientController.java
+++ src/main/java/org/example/gestionpatients/web/PatientController.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import org.example.gestionpatients.entities.Patient;
 import org.example.gestionpatients.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 import java.util.List;
@@ -18,9 +21,10 @@ public class PatientController {
     @Autowired
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(Model model){
-        List<Patient> patientList = patientRepository.findAll();
-        model.addAttribute("listPatients", patientList);
+    public String index(Model model, @RequestParam(name = "p",defaultValue = "0")int page,@RequestParam(name = "s")int size){
+        Page<Patient> pagePatient = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("listPatients", pagePatient.getContent());
+        model.addAttribute("pages", new int[pagePatient.getTotalPages()]);
         return "Patients";
     }
 }
